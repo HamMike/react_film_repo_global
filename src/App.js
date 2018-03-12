52157e9771929a038119ec45ce9001e67dfbf942
Michael Hammer <michaelhammer@Michaels-MacBook-Pro.local>
2018-03-12 09:13:48 -0700

    finished film repo

--- src/App.js
+++ src/App.js
@@ -7,11 +7,55 @@ import TMDB from './TMDB'
 const films = TMDB.films
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      films,
+      faves: [],
+      current: {}
+    }
+    this.handleFaveToggle = this.handleFaveToggle.bind(this)
+    this.handleDetailsClick = this.handleDetailsClick.bind(this)
+  }
+
+  handleFaveToggle(film) {
+    // making a copy of the array
+    const faves = this.state.faves.slice()
+    // film index is set to the index of film
+    const filmIndex = faves.indexOf(film)
+    if (filmIndex !== -1) {
+      // the film is already faved
+      // remove from the fav array
+      faves.splice(filmIndex, 1)
+      console.log('removing a favorite', film.title)
+    } else {
+      // the film needs to be added
+      faves.push(film)
+      console.log('adding a favorite', film.title)
+    }
+    this.setState({faves})
+  }
+
+  handleDetailsClick = (film) => {
+    console.log('fetching details for: ' + film.title)
+    const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB.api_key}&append_to_response=videos,images&language=en`
+    fetch(url).then(response => {
+      response.json().then(data => {
+        console.log(data) // take a look at what you get back!
+        this.setState({current: data})
+      })
+    })
+    this.setState({
+      current: film
+    })
+  }
+
   render() {
     return (
       <div className="film-library">
-        <FilmListing films={films}/>
-        <FilmDetails films={films}/>
+        <FilmListing faves={this.state.faves} onDetailsClick={this.handleDetailsClick} onFaveToggle={this.handleFaveToggle} films={this.state.films}  />
+        <FilmDetails film={this.state.current} />
       </div>
     );
   }
